a283a135ddd75b97356504a993f0914995b5bba0
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2023-08-21 13:16:04 +0200

    Add validation for form

--- client/js/index.js
+++ client/js/index.js
@@ -3,7 +3,10 @@ const userLName = document.querySelector('#valueT');
 const userEmail = document.querySelector('#valueTwo');
 const userCell = document.querySelector('#valueThree');
 const toets = document.querySelector('#toets');
-const nameError = document.querySelector('#nameError');
+const nameFError = document.querySelector('#fnameError');
+const nameLError = document.querySelector('#lnameError');
+const emailError = document.querySelector('#enameError');
+const cellError = document.querySelector('#cnameError');
 
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
 const cellRegex = /^0\d{9}$/
@@ -16,20 +19,33 @@ function showVal(event) {
 }
 
 
-function validateName() {
+function validateFName() {
 
     if (userFName.validity.tooShort) {
-        nameError.innerHTML = 'Full name must be at least 5 characters!';
-        nameError.style.color = 'red';
-        nameError.style.fontSize = '0.7rem';
+        nameFError.innerHTML = 'First name must be at least 5 characters!';
+        nameFError.style.color = 'red';
+        nameFError.style.fontSize = '0.7rem';
         
     } else {
-        nameError.innerHTML = ''; // Clear the error message
+        nameFError.innerHTML = ''; // Clear the error message
     }
 }
 
-userFName.addEventListener('blur',validateName);
+userFName.addEventListener('blur',validateFName);
 
+function validateLName() {
+
+    if (userLName.validity.tooShort) {
+        nameLError.innerHTML = 'Last name must be at least 5 characters!';
+        nameLError.style.color = 'red';
+        nameLError.style.fontSize = '0.7rem';
+        
+    } else {
+        nameLError.innerHTML = ''; // Clear the error message
+    }
+}
+
+userLName.addEventListener('blur',validateLName);
 
 function validateEmail() {
 
@@ -61,6 +77,3 @@ function testScript() {
     console.log("JavaScript is connected!");
 }
 
-// Rest of your JavaScript code
-
-// Call the test function
